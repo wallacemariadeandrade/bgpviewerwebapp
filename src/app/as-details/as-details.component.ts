ab4b2abc6fb623688b3e8bb7c40193e6326b5a81
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2021-11-22 08:02:09 -0300

    Adds model

--- src/app/as-details/as-details.component.ts
+++ src/app/as-details/as-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import AsDetails from '../model/as-details';
 
 @Component({
   selector: 'app-as-details',
@@ -6,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./as-details.component.css']
 })
 export class AsDetailsComponent implements OnInit {
+
+  details: AsDetails = {
+    asn: 3356,
+    name: "LEVEL3",
+    description: "Level 3 Parent, LLC",
+    countryCode: "US",
+    emailContacts: [
+      "abuse@aup.lumen.com",
+      "ipaddressing@level3.com",
+      "abuse@level3.com"
+    ],
+    abuseContacts: [
+      "abuse@aup.lumen.com",
+      "abuse@level3.com"
+    ],
+    lookingGlassUrl: "https://lookingglass.centurylink.com/"
+  };
 
   constructor() { }
 
---
+++ src/app/model/as-details.ts
@@ -0,0 +1,9 @@
+export default class AsDetails {
+    public asn?: Number;
+    public name?: String;
+    public description?: String;
+    public countryCode?: String;
+    public emailContacts?: String[];
+    public abuseContacts?: String[];
+    public lookingGlassUrl?: String;
+}
