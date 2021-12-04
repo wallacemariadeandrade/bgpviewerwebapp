9b38f36ea64e124fb5e2359252f7ebe7ebdeb8c1
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2021-12-04 07:23:48 -0300

    adds service

---
+++ src/app/model/prefix.ts
@@ -0,0 +1,5 @@
+export default class Prefix {
+    prefix?: string;
+    name?: string;
+    description?: string;
+}
---
+++ src/app/model/search.ts
@@ -0,0 +1,8 @@
+import AsDetails from "./asDetails";
+import Prefix from "./prefix";
+
+export default class Search {
+    relatedAsns?: AsDetails[];
+    iPv4?: Prefix[];
+    iPv6?: Prefix[];
+}
