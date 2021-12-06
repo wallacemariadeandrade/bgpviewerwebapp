e7bd729c11fe7d5c2305995c94216d0b0ef07a47
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2021-12-06 07:43:41 -0300

    refactors to inherit base class

--- src/app/prefixResultsComponent.ts
+++ src/app/prefixResultsComponent.ts
@@ -4,30 +4,25 @@ import { ApiProviderService } from "./api-provider.service";
 import SelectedApi from "./model/selectedApi";
 import { ParamsService } from "./params.service";
 import { PrefixService } from "./prefix.service";
+import { ResultsComponent } from "./resultsComponent";
 
 @Component ({
     template: ''
 })
-export abstract class PrefixResultsComponent<T> implements OnInit {
+export abstract class PrefixResultsComponent<T> extends ResultsComponent<T> {
     
-    results: [SelectedApi, T][];
     prefix: string;
 
     constructor(
-        protected paramsService: ParamsService,
-        protected apiProvider: ApiProviderService,
+        paramsService: ParamsService,
+        apiProvider: ApiProviderService,
         protected prefixService: PrefixService) {
-            this.results = [];
+            super(paramsService, apiProvider);
             this.prefix = this.paramsService.getQueryParam();
     }
 
-    ngOnInit(): void {
-        this.execute();
-    }
-
-    execute():void {
+    doWork(apisToQuery:SelectedApi[]):void {
         this.prefix = this.paramsService.getQueryParam();
-        let apisToQuery = this.apiProvider.getSelectedApis();
         apisToQuery.forEach(selected => {
             this.getData(selected.api?.id, this.prefix).subscribe(
                 x => this.results?.push([selected, x]))
